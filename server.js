21c5dc58f53653ae36fcb2fa96718308a732af33
Alexis Arando <lex4736@gmail.com>
2021-02-05 22:04:54 -0800

    added middleware for routes

--- server.js
+++ server.js
@@ -2,19 +2,14 @@ const express = require("express")
 const mongoose = require("mongoose")
 const logger = require("morgan")
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
-
-app.listen(PORT, () => {
-    console.log(`app running on port https://localhost:${PORT}`)
-});
 app.use(express.static("public"));
+app.use(require("./routes/apiroute"));
+app.use(require("./routes/htmlroute"));
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout',
@@ -25,3 +20,7 @@ mongoose.connect(
       useFindAndModify: false
     }
   );
+
+app.listen(PORT, () => {
+    console.log(`app running on port https://localhost:${PORT}`)
+});
